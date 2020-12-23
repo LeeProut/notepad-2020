a8266a8bd1661ba7525d4ea7ec003b247f0c8c7f
Lee Prout <wleeprout@gmail.com>
2020-12-22 21:57:49 -0500

    add mongo integration

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
 from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from flask_pymongo import PyMongo
 from os import environ
 
 app = Flask(__name__)
+
+app.config["MONGO_URI"] = environ.get("MONGODB_URI", "mongodb://localhost:27017/notepad2020")
+
+mongo = PyMongo(app)
+
 app.config["SQLALCHEMY_DATABASE_URI"] = environ.get("DATABASE_URL", "sqlite:///notepad-2020.sqlite ")
 
 db = SQLAlchemy(app)
@@ -29,6 +35,19 @@ def tasksPostGres():
         data.append(item)
 
     return jsonify(data)   
+
+@app.route('/api/tasks-mongo')
+def tasksMongo(): 
+    tasks = mongo.db.tasks.find({}) 
+    data = []
+
+    for task in tasks: 
+        item = {
+            '_id': str(task['_id']),
+            'description': task['description']
+        }   
+        data.append(item)
+    return jsonify(data)    
 
 if __name__ == '__main__':
     app.run(debug=True)
